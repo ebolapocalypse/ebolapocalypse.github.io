5b75aa29e840dd79b563be2c35babb26fe0fc661
Philip Mathieu <philip.eng.mathieu@gmail.com>
2014-11-01 17:49:41 -0400

    Yo yo

--- locyo.js
+++ locyo.js
@@ -1,11 +1,5 @@
 // (Location operations)
 var region = 'Northeast';
-document.getElementById('yo').innerHTML = '<h2>Want to be notified when there\'s Ebola news in the ' + region + '? *Yo* us at Ebola' + region + '!</h2><p style="font-size=10px;">(Click to close.)</p>';
-move('.popup')
-    .set('height','auto')
-    .set('top','0px')
-    .duration('1s')
-    .end();
 var x = document.getElementById('debug');
 function getLocation() {
     if (navigator.geolocation) {
@@ -69,6 +63,12 @@ function getRegion() {
 getLocation();
 getRegion();
 x.innerHTML = region;
+document.getElementById('yo').innerHTML = '<h2>Want to be notified when there\'s Ebola news in the ' + region + '? *Yo* us at Ebola' + region + '!</h2><p style="font-size=10px;">(Click to close.)</p>';
+move('.popup')
+    .set('height','auto')
+    .set('top','0px')
+    .duration('1s')
+    .end();
 document.getElementById('yo').onclick = function(e) {
     move('.popup')
         .set('top','-250px')
