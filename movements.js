a6d90d851f5fde5d802d180cfe17ffb37ee49da3
Philip Mathieu <philip.eng.mathieu@gmail.com>
2014-11-01 15:40:48 -0400

    Updated document embedding

--- movements.js
+++ movements.js
@@ -8,5 +8,17 @@ document.getElementById('nav-div').onclick = function(e) {
         .set('font-size','20px')
         .duration('.5s')
         .end();
-    move('.content').set('background-color','rgba(255,255,255,0.8').end();
+    move('.wrapper').set('background-color','rgba(255,255,255,0.8').end();
 };
+document.getElementById('learn').onclick = function(e) {
+    document.getElementById("main").innerHTML = '<object type="text/html" class="content" data="learn.html" ></object>';
+}
+document.getElementById('survive').onclick = function(e) {
+    document.getElementById("main").innerHTML = '<object type="text/html" class="content" data="survive.html" ></object>';
+}
+document.getElementById('avoid').onclick = function(e) {
+    document.getElementById("main").innerHTML = '<object type="text/html" class="content" data="avoid.html" ></object>';
+}
+document.getElementById('rogue').onclick = function(e) {
+    document.getElementById("main").innerHTML = '<object type="text/html" class="content" data="rogue.html" ></object>';
+}
