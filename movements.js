94d4e5583858fbc2a45112c4f2b5a21ef2a4d459
Philip Mathieu <philip.eng.mathieu@gmail.com>
2014-11-01 12:37:15 -0400

    Fuckin wit shit

--- movements.js
+++ movements.js
@@ -1,8 +1,17 @@
 document.getElementById('nav-div').onclick = function(e) {
-  move('.nav')
-    .set('left', 0)
-    .set('top', '15px')
-    .set('margin-left', 0)
-    .duration('.5s')
-    .end();
+    move('.nav')
+        .set('left', 0)
+        .set('top', '15px')
+        .set('margin-left', 0)
+        .set('text-align','left')
+        .duration('.5s')
+        .end();
+    move('.content').set('background-color','rgba(255,255,255,0.8').end();
 };
+// function toggle_visibility('learn-div') {
+//    var e = document.getElementById('learn-div');
+//    if(e.style.display == 'block')
+//       e.style.display = 'none';
+//    else
+//       e.style.display = 'block';
+// }
